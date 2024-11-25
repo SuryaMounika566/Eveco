ab4e594ae9bda011c775bef2a6d3e84c558d022c
Reshma-Madala <m.vijayareshma@gmail.com>
2024-11-25 10:28:05 +0530

    Created the Front End For Login and Registration Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Intial from './LoginAndRegistration/Intial'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div>
-
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intial/>}> </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
